1250ce7703aa2fc0e117b5d680394058284409f9
UrielCandelas <candelas.meza.uriel@gmail.com>
2024-04-19 16:58:23 -0600

    modificaciones en el back

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
@@ -7,65 +7,64 @@ import cookieParser from "cookie-parser";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 
-import authRoutes from './routes/auth.routes'
-import locationRoutes from './routes/locations.routes'
-import travelsRoutes from './routes/travels.routes'
-import userRoutes from './routes/users.routes'
+import authRoutes from "./routes/auth.routes";
+import locationRoutes from "./routes/locations.routes";
+import travelsRoutes from "./routes/travels.routes";
+import userRoutes from "./routes/users.routes";
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-    credentials: true,
-  },
+	cors: {
+		origin: "*",
+		credentials: true,
+	},
 });
 const origins = ["http://localhost:5173", "http://localhost:8080"];
 
 app.use(express.json());
-app.use(
-  cors({
-    origin(requestOrigin, callback) {
-      if (!requestOrigin || origins.includes(requestOrigin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+/*app.use(
+	cors({
+		origin(requestOrigin, callback) {
+			if (!requestOrigin || origins.includes(requestOrigin)) {
+				callback(null, true);
+			} else {
+				callback(new Error("Not allowed by CORS"));
+			}
+		},
+		credentials: true,
+	})
+);*/
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(cookieParser());
 
-
-app.use('/api', authRoutes)
-app.use('/api', locationRoutes)
-app.use('/api', travelsRoutes)
-app.use('/api', userRoutes)
+app.use("/api", authRoutes);
+app.use("/api", locationRoutes);
+app.use("/api", travelsRoutes);
+app.use("/api", userRoutes);
 
 global.onlineUsers = new Map();
 
 io.on("connection", async (socket) => {
-  global.chatSocket = socket;
-  socket.on("add-user", (userId) => {
-    global.onlineUsers.set(userId, socket.id);
-  });
+	global.chatSocket = socket;
+	socket.on("add-user", (userId) => {
+		global.onlineUsers.set(userId, socket.id);
+	});
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = global.onlineUsers.get(data.to);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.message);
-    }
-  });
+	socket.on("send-msg", (data) => {
+		const sendUserSocket = global.onlineUsers.get(data.to);
+		if (sendUserSocket) {
+			socket.to(sendUserSocket).emit("msg-recieve", data.message);
+		}
+	});
 
-  socket.on("send-requests", (data) => {
-    const sendUserSocket = global.onlineUsers.get(data.id);
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("get-requests", data.req);
-    }
-  });
+	socket.on("send-requests", (data) => {
+		const sendUserSocket = global.onlineUsers.get(data.id);
+		if (sendUserSocket) {
+			socket.to(sendUserSocket).emit("get-requests", data.req);
+		}
+	});
 });
 
 export default httpServer;
