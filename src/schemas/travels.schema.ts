7781f30795463e963c58d38c868e0ec8fc3d92a0
UrielCandelas <candelas.meza.uriel@gmail.com>
2024-06-01 23:30:05 -0600

    Solucion de bugs y textos para las modales

--- src/schemas/travels.schema.ts
+++ src/schemas/travels.schema.ts
@@ -7,11 +7,27 @@ export const createTravelSchema = z.object({
 	id_location: z.string({
 		required_error: "El id de la locacion es requerido",
 	}),
-	travel_date: z.string({ required_error: "La fecha es requerida" }),
-	expense: z.string({ required_error: "El gasto es requerido" }),
-	quantity: z.number({ required_error: "La cantidad es requerida" }),
+	travel_date: z
+		.string({
+			required_error: "La fecha es requerida",
+			invalid_type_error: "La fecha es requerida",
+		})
+		.min(10, {
+			message: "Ingrese la fecha",
+		}),
+	expense: z.string({ required_error: "El gasto es requerido" }).min(5, {
+		message: "El gasto es requerido",
+	}),
+	quantity: z.number({
+		required_error: "La cantidad es requerida",
+		invalid_type_error: "El gasto debe ser un valor numerico",
+	}),
 	extra: z.string().optional(),
-	companions: z.number({ required_error: "Los acompañantes son requeridos" }),
+	companions: z.number({
+		required_error: "Los acompañantes son requeridos",
+		invalid_type_error:
+			"La cantidad de acompañantes deben ser un valor numerico",
+	}),
 });
 
 export const travelSchema = z.object({
@@ -23,5 +39,9 @@ export const travelSchema = z.object({
 	id_transportation: z.number({
 		required_error: "El id del trasnporte es requerido",
 	}),
-	companions: z.number({ required_error: "Los compañeros son requeridos" }),
+	companions: z.number({
+		required_error: "Los acompañantes son requeridos",
+		invalid_type_error:
+			"La cantidad de acompañantes deben ser un valor numerico",
+	}),
 });
